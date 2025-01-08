410589369ca4567c648f2571f794379b05b55070
ar00ster18 <arielarusy12@gmail.com>
2025-01-08 16:28:43 +0200

    Take 2

--- User.java
+++ User.java
@@ -44,7 +44,7 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         for (int i = 0; i < fCount; i++){
-            if (follows[i].equals(name)){
+            if (this.follows[i].equals(name)){
                 return true;
             }
         }
@@ -53,7 +53,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (!follows(name) && fCount < 10){
+        if (!follows(name) && fCount < maxfCount){
             follows[fCount] = name;
             fCount++;
             return true;
